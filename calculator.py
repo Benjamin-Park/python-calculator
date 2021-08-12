b51a9f10801084076e729f12d773c60535117295
Benjamin Park <36532131+Benjamin-Park@users.noreply.github.com>
2021-08-13 11:00:08 +1200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,26 @@
 import tkinter as tk
 
+
 class Calculator(tk.Frame):
     def __init__(self, parent):
         self.parent = parent
         parent.title("Calculator")
         parent.geometry("150x200")
         parent.resizable(False, False)
+
+        self.equation = tk.StringVar()
+        self.entry = tk.Entry(self.parent, textvariable=self.equation)
+        # FIXME: Replace with grid
+        self.entry.pack()
+        
+        parent.bind('<Delete>', self.clear)
+
+    def clear(self):
+        self.equation.set("")
+
+    # TODO: Implement eval sanitization
+    def solve(self):
+        self.equation.set(eval(self.equation))
 
 
 root = tk.Tk()

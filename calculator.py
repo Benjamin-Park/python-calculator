c4c54cf4afdf10daf6c3a7376e0d83b8a53c6dd8
Benjamin-Park <36532131+Benjamin-Park@users.noreply.github.com>
2021-08-14 14:39:37 +1200

    Added eval input sanitization

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import re
 
 
 class Calculator(tk.Frame):
@@ -12,15 +13,35 @@ class Calculator(tk.Frame):
         self.entry = tk.Entry(self.parent, textvariable=self.equation)
         # FIXME: Replace with grid
         self.entry.pack()
-        
-        parent.bind('<Delete>', self.clear)
 
-    def clear(self):
+        self.button_clear = tk.Button(parent, text="CE", command=self.clear)
+        self.button_clear.pack()
+        self.button_0 = tk.Button(parent, text="0", command=lambda: self.button_press("0"))
+        self.button_0.pack()
+        self.button_1 = tk.Button(parent, text="1", command=lambda: self.button_press("1"))
+        self.button_1.pack()
+        self.button_plus = tk.Button(parent, text="+", command=lambda: self.button_press("+"))
+        self.button_plus.pack()
+        self.button_solve = tk.Button(parent, text="=", command=self.solve)
+        self.button_solve.pack()
+
+        # Bind keys to functions to allow keyboard input
+        parent.bind('<Delete>', self.clear)
+        parent.bind('<Return>', self.solve)
+
+    def clear(self, event=None):
         self.equation.set("")
 
-    # TODO: Implement eval sanitization
-    def solve(self):
-        self.equation.set(eval(self.equation))
+    def button_press(self, key_value, event=None):
+        if re.match("^[0-9()+\-*/.]*$", key_value):
+            self.equation.set(self.equation.get() + key_value)
+
+    def solve(self, event=None):
+        if self.equation.get() != "":
+            try:
+                self.equation.set(eval(self.equation.get()))
+            except SyntaxError:
+                print("Syntax Error")
 
 
 root = tk.Tk()
